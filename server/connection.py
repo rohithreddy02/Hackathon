8b2089931e5ef855f57ebf5026ba6da1b25a6c76
Dheemanth1910 <dheemanth1910@gmail.com>
2023-03-23 20:50:24 +0530

    SDLJkasdlfkjsfda

--- server/connection.py
+++ server/connection.py
@@ -1,6 +1,6 @@
-import mysql.connector
+# import mysql.connector
 
-def create_connection():
-    db = mysql.connector.connect(host='localhost', user='root',
-                             password='12345678', database='studentinformation', port='330')
-    return db
+# def create_connection():
+#     db = mysql.connector.connect(host='localhost', user='root',
+#                              password='12345678', database='studentinformation', port='330')
+#     return db
