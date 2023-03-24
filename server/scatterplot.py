489e2b65127682e94637c7fd5dc7a8f499cb562b
Your Name <you@example.com>
2023-03-24 20:09:53 +0530

    added databse fields visualizations changed

--- server/scatterplot.py
+++ server/scatterplot.py
@@ -5,15 +5,15 @@ from connection import create_connection
 db = create_connection()
 colors = ['red', 'green', 'blue', 'orange', 'yellow', 'violet']
 # barWidth = 0.5
-df = pd.read_sql('select * from studentperformance', con=db)
+df = pd.read_sql('select * from Studentdetails', con=db)
 
 # fig, ax = plt.subplots()
 # bars = ax.bar(df['name'], df['gpa'], color=colors, width=barWidth)
 
 # plt.show()
 
-plt.scatter(df['cocurricular'], df['gpa'], alpha=0.75, c=df['extracurricular'],
-            marker='o', cmap='viridis', s=25*df['extracurricular'] + 150, edgecolors='gray')
+plt.scatter(df['Nocert'], df['TotalGpa'], alpha=0.75, c=df['Extra'],
+            marker='o', cmap='viridis', s=25*df['Extra'] + 150, edgecolors='gray')
 plt.title('Student Overall performance', fontsize=20)
 plt.xlabel('Student cocurricular activities ---->', fontsize=12)
 plt.ylabel('Student Grades ---->', fontsize=12)
