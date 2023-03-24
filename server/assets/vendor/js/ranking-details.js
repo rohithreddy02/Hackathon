9fb6d56ae8de7517faaad17375f181f32c60ad5e
Dheemanth1910 <dheemanth1910@gmail.com>
2023-03-24 21:10:51 +0530

    added adminranking and user ranking pages

---
+++ server/assets/vendor/js/ranking-details.js
@@ -0,0 +1,31 @@
+clusterDisplay=document.querySelector('#ranking-table')
+fetch('http://localhost:3000/ranking')
+  .then(response => response.text())
+  .then(data => {
+    const jsonData = JSON.parse(data);
+    const table = document.createElement("table");
+    table.setAttribute("class", "table")
+    // Create table headers
+    const headers = table.createTHead();
+    const headerRow = headers.insertRow(0);
+    Object.keys(jsonData[0]).forEach(key => {
+      if (key != 'Total'){
+        const th = document.createElement("th");
+        th.innerText = key;
+        headerRow.appendChild(th);
+      }
+    });
+
+    // Create table rows for data
+    jsonData.forEach(element => {
+      const row = table.insertRow();
+      Object.keys(element).forEach(key => {
+        if(key!='Total'){
+            const cell = row.insertCell();
+            cell.innerHTML = element[key];
+        }
+      });
+    });
+
+    clusterDisplay.appendChild(table);
+  });
